8c4e4ab6b5890755f3e126c51a35a949621ec854
Andrew Leach <andrewleachy@gmail.com>
2023-03-16 09:33:47 -0700

    Add text fields

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:calendar_date_picker2/calendar_date_picker2.dart';
 import 'package:coconut_chronicles/widgets/home_page_drawer.dart';
 import 'package:coconut_chronicles/widgets/home_page_fab.dart';
 import 'package:flutter/material.dart';
@@ -18,7 +19,44 @@ class HomePageState extends State<HomePage> {
       ),
       floatingActionButton: const HomePageFab(),
       drawer: const HomePageDrawer(),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.only(bottom: 32, left: 32, right: 32),
+        child: Column(
+          children: [
+            TextButton(
+              onPressed: () => showCalendarDatePicker2Dialog(
+                context: context,
+                config: CalendarDatePicker2WithActionButtonsConfig(),
+                dialogSize: const Size(325, 400),
+                initialValue: [],
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: const Text('Select Date'),
+            ),
+            const SizedBox(height: 16),
+            // We want a title and body input field, with the body filling the remaining space
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Title',
+              ),
+            ),
+            const SizedBox(height: 16),
+            const Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Body',
+                ),
+                maxLines: null,
+                expands: true,
+                keyboardType: TextInputType.multiline,
+                textCapitalization: TextCapitalization.sentences,
+              ),
+            ),
+          ]
+        ),
+      )
     );
   }
 
