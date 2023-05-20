d2d2a7c115f5e55151d4c0105d29091f898c52a5
Andrew L <anleac@github.com>
2023-05-20 11:55:44 +0200

    Spiking

--- lib/core/helpers/entry_helper.dart
+++ lib/core/helpers/entry_helper.dart
@@ -19,4 +19,46 @@ class EntryHelper {
 
     return canSelect;
   }
+
+  (String sanitisedSegment, List<(String segment, int index)> hiddenSegments) processParagraph(String paragraph) {
+    String sanitised = '';
+    List<(String, int)> hiddenSegments = [];
+    bool isHidden = false;
+    StringBuffer hiddenBuffer = StringBuffer();
+    int index = 0;
+
+    // Loop through each character in the original paragraph
+    for (int i = 0; i < paragraph.length; i++) {
+      // Get the current character
+      String char = paragraph[i];
+
+      // If the character is ~, check the next two characters
+      if (char == '~') {
+        // If the next two characters are also ~, toggle the isHidden flag and skip them
+        if (i + 2 < paragraph.length && paragraph[i + 1] == '~' && paragraph[i + 2] == '~') {
+          isHidden = !isHidden;
+          i += 2;
+          continue;
+        }
+      }
+
+      // If we are in a hidden segment, append the character to the hidden buffer
+      if (isHidden) {
+        hiddenBuffer.write(char);
+      } else {
+        // Otherwise, append the character to the sanitised paragraph and increment the index
+        sanitised += char;
+        index++;
+      }
+
+      // If we are at the end of a hidden segment, add the hidden buffer and the index to the hidden list
+      // and clear the hidden buffer
+      if (isHidden && (i == paragraph.length - 1 || paragraph[i + 1] == '~')) {
+        hiddenSegments.add((hiddenBuffer.toString(), index));
+        hiddenBuffer.clear();
+      }
+    }
+
+    return (sanitised, hiddenSegments);
+  }
 }
